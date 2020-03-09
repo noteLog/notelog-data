aabbbb02d5316fc17801025d3343a906d7e1d52a
tansawit <sawit.tr@gmail.com>
2020-03-09 13:46:13 +0700

    updated: main

--- main.go
+++ main.go
@@ -2,26 +2,18 @@ package main
 
 import (
 	"flag"
-	"log"
 )
 
 func main() {
 	var esURL = flag.String("esurl", "", "URL of your Elasticsearch service")
 	var esPWD = flag.String("espwd", "", "Password for your Elasticsearch service")
-	var ghToken = flag.String("ghtoken", "", "GitHub Access Token")
+	// var ghToken = flag.String("ghtoken", "", "GitHub Access Token")
 	flag.Parse()
 
 	// gocron.Every(1).Hour().Do(githubCronPlaceholder)
 	// gocron.Every(1).Day().Do(blogCronPlaceholder)
 	// <-gocron.Start()
 
-	esIndexGitHub(*esURL, *esPWD, *ghToken)
-}
-
-func githubCronPlaceholder() {
-	log.Printf("Fetching GitHub Repos")
-}
-
-func blogCronPlaceholder() {
-	log.Printf("Fetching Blog Posts")
+	// esIndexGitHub(*esURL, *esPWD, *ghToken)
+	esIndexBlog(*esURL, *esPWD)
 }
